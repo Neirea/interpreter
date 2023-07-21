2112b66f670045b6e8a951820bac77425a06de72
Neirea <neireasoultix@gmail.com>
2023-07-22 01:53:40 +0300

    integer evaluator

--- go/repl/repl.go
+++ go/repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"lang/evaluator"
 	"lang/lexer"
 	"lang/parser"
 )
@@ -27,8 +28,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 
 	}
 }
