7c432705a7535a95f6e3ece71c33fec6aa616d83
Neirea <neireasoultix@gmail.com>
2023-07-22 02:07:25 +0300

    added remaining primitives to evaluator
    - float
    - boolean
    - null

--- go/evaluator/evaluator.go
+++ go/evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"lang/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
@@ -15,7 +21,12 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.FloatLiteral:
+		return &object.Float{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
+
 	return nil
 }
 
@@ -25,4 +36,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
