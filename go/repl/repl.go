f964441a5ba5290358a0fd700495e2db60de514d
Neirea <neireasoultix@gmail.com>
2023-08-03 13:33:35 +0300

    let statements (identifiers)

--- go/repl/repl.go
+++ go/repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"lang/evaluator"
 	"lang/lexer"
+	"lang/object"
 	"lang/parser"
 )
 
@@ -13,6 +14,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 	for {
 		fmt.Fprintf(out, "%v", PROMPT)
 		scanned := scanner.Scan()
@@ -28,7 +30,7 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
