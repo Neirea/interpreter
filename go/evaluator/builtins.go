7e9f6638b638a4f624978d1a927d9a9106ece65c
Neirea <neireasoultix@gmail.com>
2023-08-07 21:56:41 +0300

    added print built-in function

--- go/evaluator/builtins.go
+++ go/evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "lang/object"
+import (
+	"fmt"
+	"lang/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -91,6 +94,14 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"print": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
