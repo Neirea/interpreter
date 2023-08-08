0cf6d68f41a41cd78b81018a7f46d9a1f723c79b
Neirea <neireasoultix@gmail.com>
2023-08-09 02:41:26 +0300

    added read from file functionality
    - mirroring functionality from Golang's version
    - added 'npm run build' command and
    - added other npm start:* commands
    - added 'example.mlg' file

---
+++ ts/src/index.ts
@@ -0,0 +1,43 @@
+import os from "node:os";
+import { printParserErrors, replStart } from "./repl";
+import * as fs from "node:fs/promises";
+import { Lexer } from "./lexer";
+import { Parser } from "./parser";
+import { evalCode } from "./evaluator";
+import { Environment } from "./object/enviroment";
+
+(async () => {
+    const fileFlagIndex = process.argv.indexOf("-f");
+    if (fileFlagIndex !== -1 && fileFlagIndex + 1 < process.argv.length) {
+        const filePath = process.argv[fileFlagIndex + 1];
+        const input = await fs.readFile(filePath, "utf-8");
+
+        const env = Environment.new();
+        const lexer = new Lexer(input);
+        const parser = new Parser(lexer);
+        const program = parser.parseProgram();
+
+        if (parser.errors.length > 0) {
+            printParserErrors(parser.errors);
+            return;
+        }
+        const evaluated = evalCode(program, env);
+        if (evaluated !== undefined) {
+            console.log(evaluated.inspect());
+        }
+        return;
+    }
+    // Check for other flags or arguments
+    const otherFlags = process.argv.filter((arg, index) => {
+        return arg.startsWith("-") && index !== fileFlagIndex;
+    });
+
+    if (otherFlags.length > 0) {
+        console.log("Unexpected flags or arguments:", otherFlags);
+        process.exit(1);
+    }
+    const user = os.userInfo().username;
+    console.log(`Hello ${user}! This is the Monkey programming language!`);
+    console.log("Feel free to type in commands");
+    replStart();
+})();
--- ts/src/repl/index.ts
+++ ts/src/repl/index.ts
@@ -1,40 +1,38 @@
 import readline from "readline";
 import { Lexer } from "../lexer";
 import { Parser } from "../parser";
-import os from "node:os";
 import { Environment } from "../object/enviroment";
 import { evalCode } from "../evaluator";
 
-const rs = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: ">> ",
-});
+export function replStart() {
+    const rs = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: ">> ",
+    });
 
-const env = Environment.new();
+    const env = Environment.new();
 
-const user = os.userInfo().username;
-console.log(`Hello ${user}! This is the Monkey programming language!`);
-console.log("Feel free to type in commands");
-rs.prompt();
-
-rs.on("line", (input) => {
-    const lexer = new Lexer(input);
-    const parser = new Parser(lexer);
-    const program = parser.parseProgram();
-
-    if (parser.errors.length > 0) {
-        printParserErrors(parser.errors);
-        return;
-    }
-    const evaluated = evalCode(program, env);
-    if (evaluated !== undefined) {
-        console.log(evaluated.inspect());
-    }
     rs.prompt();
-});
 
-function printParserErrors(errors: string[]) {
+    rs.on("line", (input) => {
+        const lexer = new Lexer(input);
+        const parser = new Parser(lexer);
+        const program = parser.parseProgram();
+
+        if (parser.errors.length > 0) {
+            printParserErrors(parser.errors);
+            return;
+        }
+        const evaluated = evalCode(program, env);
+        if (evaluated !== undefined) {
+            console.log(evaluated.inspect());
+        }
+        rs.prompt();
+    });
+}
+
+export function printParserErrors(errors: string[]) {
     console.error("parser errors:");
     for (const error of errors) {
         console.error(`\t${error}`);
